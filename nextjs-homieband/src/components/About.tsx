01a04444c8b8a9e73a398dec58fdec8cd5d99bcc
chepchep <trungndt92@gmail.com>
2025-05-22 15:15:27 +0700

    style About Us

--- nextjs-homieband/src/components/About.tsx
+++ nextjs-homieband/src/components/About.tsx
@@ -7,17 +7,16 @@ export default function About() {
   return (
     <section id="about" className="py-20 px-5 xl:px-20">
       <div className="container">
-        <div className="section-title align-left">
-          <h2>Về HomieBand</h2>
-          <span>IT’S HOMIE</span>
-        </div>
         <div className="mx-auto px-4 grid grid-cols-1 md:grid-cols-2 gap-6 items-center">
           <div className="md:col-span-1">
             <img src={urlFor(data.image).url()} alt="Descriptive alt" className="w-auto h-[500px] rounded" />
           </div>
 
-          <div className="md:col-span-1 space-y-4">
-            <h2>{data.title}</h2>
+          <div className="md:col-span-1">
+            <div className="section-title align-left">
+              <h2>Về HomieBand</h2>
+              <span>IT’S HOMIE</span>
+            </div>
             <p className="mt-2 text-xl">{data.description}</p>
           </div>
         </div>
