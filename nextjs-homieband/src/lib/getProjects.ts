19cfc565ad2d4c51f20e5f4cbaac0a12bbe46db9
chepchep <trungndt92@gmail.com>
2025-07-08 12:57:50 +0700

    remove unused preview code

--- nextjs-homieband/src/lib/getProjects.ts
+++ nextjs-homieband/src/lib/getProjects.ts
@@ -1,8 +1,8 @@
 // lib/getProjects.ts
 import { client } from '@/sanity/client'
-export async function getProjects(isPreview = false) {
+export async function getProjects() {
   const query = `*[_type == "project"]`
   return client.fetch(query, {}, {
-    perspective: isPreview ? 'previewDrafts' : 'published',
+    perspective: 'published',
   })
 }
